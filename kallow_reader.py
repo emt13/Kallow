d428e598c810a1b1a3375fd70c642fb6e0c09b9f
Alex Vlissidis <alex@airbears2-10-142-67-37.airbears2.1918.berkeley.edu>
2017-09-30 16:53:55 -0700

    Read everything from data/

--- kallow_reader.py
+++ kallow_reader.py
@@ -2,6 +2,6 @@ import numpy as np
 import pickle
 
 def kallow_read():
-    properties = pickle.load(open("properties.p", "rb"))
-    transactions = pickle.load(open("transactions.p", "rb"))
+    properties = pickle.load(open("data/properties.p", "rb"))
+    transactions = pickle.load(open("data/transactions.p", "rb"))
     return properties, transactions
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ import sys
 
 ''' *************************** Read in the data ****************************'''
 properties = list()
-with open("properties_2016.csv", "r") as f:
+with open("data/properties_2016.csv", "r") as f:
     reader = csv.reader(f, delimiter=",")
     next(reader) # Skip header
     for row in reader:
@@ -13,14 +13,14 @@ with open("properties_2016.csv", "r") as f:
 
 chunk_size = round(len(properties)/10)
 properties = np.array(properties[:chunk_size])
-pickle.dump(properties, open("properties.p", "wb"))
+pickle.dump(properties, open("data/properties.p", "wb"))
 
 transactions = list()
-with open("train_2016_v2.csv", "r") as f:
+with open("data/train_2016_v2.csv", "r") as f:
     reader = csv.reader(f, delimiter=",")
     next(reader) # Skip header
     for row in reader:
         transactions.append(row)
 
 transactions = np.array(transactions)
-pickle.dump(transactions, open("transactions.p", "wb"))
+pickle.dump(transactions, open("data/transactions.p", "wb"))

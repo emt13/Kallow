9726b5cd49cf267f78659e440450ea6704bd13d5
Alex Vlissidis <alex@airbears2-10-142-67-37.airbears2.1918.berkeley.edu>
2017-09-30 18:37:21 -0700

    Formatted stuff

--- kallow_reader.py
+++ kallow_reader.py
@@ -2,6 +2,7 @@ import numpy as np
 import pickle
 
 def kallow_read():
+    pids = pickle.load(open("data/pids.p", "rb"))
     properties = pickle.load(open("data/properties.p", "rb"))
     transactions = pickle.load(open("data/transactions.p", "rb"))
-    return properties, transactions
+    return pids, properties, transactions
--- setup.py
+++ setup.py
@@ -13,16 +13,19 @@ with open("data/properties_2016.csv", "r") as f:
 chunk_size = round(len(properties)/10) # Determine chunk size
 properties = np.array(properties[:chunk_size]) # Select only a small chunk
 # Select only the columns that matter
-properties = np.column_stack((properties[:, 0], properties[:, 4], properties[:, 5], properties[:, 19], properties[:, 26], properties[:, 11], properties[:, 27], properties[:, 40], properties[:, 51], properties[:, 53]))
+pids = properties[:, 0].astype(int).tolist()
+properties = np.column_stack((properties[:, 4], properties[:, 5], properties[:, 19], properties[:, 26], properties[:, 11], properties[:, 27], properties[:, 40], properties[:, 51], properties[:, 53]))
+properties[properties == ''] = 0.0 # Deal with missing elements
+properties = properties.astype(float)
 
+pickle.dump(pids, open("data/pids.p", "wb"))
 pickle.dump(properties, open("data/properties.p", "wb"))
 
-transactions = list()
+transactions = dict()
 with open("data/train_2016_v2.csv", "r") as f:
     reader = csv.reader(f, delimiter=",")
     next(reader) # Skip header
     for row in reader:
-        transactions.append(row)
+        transactions[int(row[0])] = float(row[1])
 
-transactions = np.array(transactions)
 pickle.dump(transactions, open("data/transactions.p", "wb"))

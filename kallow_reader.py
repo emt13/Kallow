eef53a4f5cc94651effc960bd2093efc6c1d0311
Alex Vlissidis <alex@Alexs-MacBook-Pro.local>
2017-10-06 21:05:03 -0700

    deleted kallow reader

--- kallow_reader.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-import pickle
-
-def kallow_read():
-    pids = pickle.load(open("data/pids.p", "rb"))
-    properties = pickle.load(open("data/properties.p", "rb"))
-    transactions = pickle.load(open("data/transactions.p", "rb"))
-    return pids, properties, transactions
